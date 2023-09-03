a57a2b739a98d46146074ecd1f53d02893603e68
mthang <m.thang@qfab.org>
2023-09-03 14:15:24 +1000

    Delete transform_nr.sh

--- transform_nr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat nr | perl -e 'BEGIN{ $/ = ">"; <>; } while(<>){s/>$//;  $i = index $_, "]\n"; $h = substr($_, 0, $i)."]"; $s = substr $_, $i+2; while($h =~ /(.+?) \[(.+?)\]/g){ print ">$1 [$2]\n$s";} } ' | sed 's/\x01//g' >> nr.fa
