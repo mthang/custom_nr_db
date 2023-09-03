cec129d9eaccdadb2fa9fe76736542cccb72611d
mthang <m.thang@qfab.org>
2023-09-03 14:08:11 +1000

    Update subset_nr.sh

--- subset_nr.sh
+++ subset_nr.sh
@@ -2,8 +2,11 @@
 
 export PATH=$PATH:/path/to/seqkit
 
+# nr.fa.gz downloaded from here https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nr.gz
 #ACCFILE=Fungi_out.csv
 #ACCFILE=Viridiplantae_out.csv
 ACCFILE=Metazoa_out.csv
 
-cat nr/nr.fa | seqkit grep -f ${ACCFILE} -o Metazoa_nr.fa
+cat <(echo) <(pigz -dc nr.fa.gz) \
+    | perl -e 'BEGIN{ $/ = "\n>"; <>; } while(<>){s/>$//;  $i = index $_, "\n"; $h = substr $_, 0, $i; $s = substr $_, $i+1; if ($h !~ />/) { print ">$_"; next; }; $h = ">$h"; while($h =~ />([^ ]+ .+?) ?(?=>|$)/g){ $h1 = $1; $h1 =~ s/^\W+//; print ">$h1\n$s";} } ' \
+    | seqkit grep -f ${ACCFILE} -o nr.viridiplantaea.fa.gz
